8f4ceee6855b16f5d4d8c22b38f8a70fdf42ba54
shoce <yogze@icloud.com>
2024-09-03 17:00:21 +0530

    sre

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shoce/sre
+
+go 1.23.0
---
+++ sre.go
@@ -0,0 +1,67 @@
+/*
+usage: sre S1 [S2]
+S1 can be a regex
+replace all occurences of S1 with S2 in stdin and print to stdout
+if S2 is not provided, all occurences of S1 are removed
+
+GoFmt
+GoBuildNull
+GoBuild
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+const (
+	NL = "\n"
+)
+
+var (
+	S1, S2 string
+	R1     *regexp.Regexp
+)
+
+func main() {
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		fmt.Fprintf(os.Stderr, "usage: sre S1 [S2]"+NL)
+		os.Exit(1)
+	}
+	S1 = os.Args[1]
+	if len(os.Args) == 3 {
+		S2 = os.Args[2]
+	}
+
+	if strings.HasPrefix(S1, "/") && strings.HasSuffix(S1, "/") {
+		s1 := strings.TrimPrefix(strings.TrimSuffix(S1, "/"), "/")
+		var err error
+		R1, err = regexp.Compile(s1)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "provided regular expression compile error:"+NL+"%v"+NL, err)
+			os.Exit(1)
+		}
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	var line1, line2 string
+	for scanner.Scan() {
+		line1 = scanner.Text()
+		if R1 == nil {
+			line2 = strings.ReplaceAll(line1, S1, S2)
+		} else {
+			line2 = R1.ReplaceAllLiteralString(line1, S2)
+		}
+		fmt.Println(line2)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "error reading input: %v"+NL, err)
+		os.Exit(1)
+	}
+}
